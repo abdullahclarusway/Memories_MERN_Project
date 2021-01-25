ce576512ff4808704ce39a56862fa32b6e4ae55d
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-01-25 01:24:56 +0100

    Initialize Project

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const bodyParser = require("body-Parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://dbUser:1234Qwer...@cluster0.i6ive.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server is running on port : ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
