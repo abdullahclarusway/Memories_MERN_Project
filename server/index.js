e6838c4f845454544da1f201668b08e651c336eb
abdullahclarusway <67835505+abdullahclarusway@users.noreply.github.com>
2021-02-02 01:18:42 +0100

    Add enviromental variables

--- server/index.js
+++ server/index.js
@@ -1,26 +1,29 @@
 const express = require("express");
-const bodyParser = require("body-Parser");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const postRoutes = require("./routes/posts");
 
 const app = express();
-
-app.use("/posts", postRoutes);
-
+require("dotenv").config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-  "mongodb+srv://dbUser:1234Qwer...@cluster0.i6ive.mongodb.net/<dbname>?retryWrites=true&w=majority";
+app.use("/posts", postRoutes);
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
-    app.listen(PORT, () => console.log(`Server is running on port : ${PORT}`))
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
   )
-  .catch((error) => console.log(error.message));
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set("useFindAndModify", false);
